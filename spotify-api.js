93f7ef13a23ad058c835bb99a87e8460cc32e468
Jeffery Shivers <jefferyshivers@gmail.com>
2017-04-22 14:28:07 -0400

    update again

--- spotify-api.js
+++ spotify-api.js
@@ -15,3 +15,19 @@ success: function () {
 
 },
 });
+
+
+$.ajax({
+        url: 'https://api.spotify.com/v1/audio-analysis/{06AKEBrKUckW0KREUWRnvT}',
+        beforeSend: function(xhr) {
+             xhr.setRequestHeader("Authorization", "Bearer BQCF3Sq0PEba47j6oH51kUtYVzQ5g2Wb1FKZaMvZoty2tE5dZtF_rKVrGeNHJjgnVk8-SJjDh7No0rBzIbUcfjA_Idg2bflm7NTRmxNnIwDN4NNx4SCM3Q3ZWksx37v15W1sQMUedlI8j6WU")
+        },
+        error: function (err) {
+          console.log("this did not work");
+        },
+        success: function(data){
+            //alert(data);
+            console.log("This worked!!!!!");
+            //process the JSON data etc
+        }
+})
